5343b721c0085060ca55501d451570569bc994fb
mohamed <76411950+mmoha-199@users.noreply.github.com>
2021-04-01 21:59:15 +0100

    adding favicon

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// Set a same-site cookie for first-party contexts
+document.cookie = 'cookie1=value1; SameSite=Lax';
+// Set a cross-site cookie for third-party contexts
+document.cookie = 'cookie2=value2; SameSite=None; Secure';
+
+
+
+
 const NAVI = document.getElementById('top-nav');
 const BTNMINIBAR = document.getElementById('icon-nav');
 const ABOUTBTN = document.getElementById('about-btn');
